d40e67687e9c6318b2edf35da6e7899c257e07a9
Paul Renvoisé <renvoisepaul@gmail.com>
2020-05-01 02:57:28 +0200

    feat(classproperty) add @classproperty to combine @classmethod and @property

--- flashback/__init__.py
+++ flashback/__init__.py
@@ -1,5 +1,6 @@
 from .borg import Borg
 
+from .classproperty import classproperty
 from .deprecated import deprecated
 from .retryable import retryable
 from .timeable import timeable
@@ -7,9 +8,10 @@ from .timeable import timeable
 
 __all__ = (
     'Borg',
+    'classproperty',
     'deprecated',
     'retryable',
     'timeable'
 )
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
