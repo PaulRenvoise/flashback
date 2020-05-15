99faf0ec9dafcfd2b056d72d1c79b2bf368fa37c
Paul Renvoisé <renvoisepaul@gmail.com>
2020-05-15 17:33:40 +0200

    doc(iterating) add missing examples

--- flashback/iterating/compact.py
+++ flashback/iterating/compact.py
@@ -2,6 +2,18 @@ def compact(iterable):
     """
     Removes None items from `iterable`.
 
+    Examples:
+        ```python
+        from flashback.iterating import compact
+
+        for user_id in compact([1058, None, 85, 9264, 19475, None]):
+            print(user_id)
+        #=> 1058
+        #=> 85
+        #=> 9264
+        #=> 19475
+        ```
+
     Params:
         - `iterable (Iterable<Any>)` the iterable to remove None from
 
