4a183ed91b3eac662d901d4bd78bca8234805e9a
Paul Renvoisé <renvoisepaul@gmail.com>
2025-11-19 16:11:09 +0100

    typing(accessing): improve typing and naming for pick and values_at

--- flashback/accessing/values_at.py
+++ flashback/accessing/values_at.py
@@ -2,26 +2,26 @@ from collections.abc import Hashable, Mapping
 import typing as t
 
 
-def values_at[T](dictionary: Mapping[t.Any, T], *keys: Hashable) -> list[T]:
+def values_at[T](mapping: Mapping[t.Any, T], *keys: Hashable) -> list[T]:
     """
-    Retrieves the values corresponding to each `keys` in `dictionary`.
+    Retrieves the values corresponding to each `keys` in `mapping`.
 
     Examples:
         ```python
         from flashback.accessing import values_at
 
         # Without values_at
-        values = [dictionary[k] for k in ["key1", "key2"] if k in dictionary]
+        values = [mapping[k] for k in ["key1", "key2"] if k in mapping]
 
         # With values_at
-        values = values_at(dictionary, "key1", "key2")
+        values = values_at(mapping, "key1", "key2")
         ```
 
     Params:
-        dictionary: the dict to fetch the values from
+        mapping: the dict to fetch the values from
         keys: the keys to get the data with
 
     Returns:
         the fetched values
     """
-    return [dictionary[k] for k in keys if k in dictionary]
+    return [mapping[k] for k in keys if k in mapping]
