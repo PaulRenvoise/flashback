b34c023cb8317b69f5c96ccee7b901c825f7f028
Paul Renvoisé <renvoisepaul@gmail.com>
2026-02-05 01:24:07 +0100

    refactor(accessing/pick): tighter typing

--- flashback/accessing/pick.py
+++ flashback/accessing/pick.py
@@ -1,8 +1,7 @@
-from collections.abc import Hashable, Mapping
-import typing as t
+from collections.abc import Mapping
 
 
-def pick[T](mapping: Mapping[t.Any, T], *keys: Hashable) -> dict[Hashable, T]:
+def pick[K, V](mapping: Mapping[K, V], *keys: K) -> dict[K, V]:
     """
     Fetches key/value pairs from `mapping` corresponding to `keys`.
 
