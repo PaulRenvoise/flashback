6ed206e960ebaf48400a21ac1f91b40b76182543
Paul Renvoisé <renvoisepaul@gmail.com>
2026-02-05 16:18:04 +0100

    typing(iterating/chunks): better return typing with/without pad

--- tests/iterating/test_chunks.py
+++ tests/iterating/test_chunks.py
@@ -7,16 +7,20 @@ class TestChunks:
         assert not chunked
 
     def test_multiple_items(self) -> None:
-        chunked = list(chunks([1, 2, 3, 4]))
-        assert chunked == [(1, 2), (3, 4)]
-
-    def test_multiple_items_without_pad(self) -> None:
         chunked = list(chunks([1, 2, 3]))
         assert chunked == [(1, 2), (3,)]
+
+    def test_multiple_items_with_size(self) -> None:
+        chunked = list(chunks([1, 2, 3], size=1))
+        assert chunked == [(1,), (2,), (3,)]
 
     def test_multiple_items_with_pad(self) -> None:
         chunked = list(chunks([1, 2, 3], pad=None))
         assert chunked == [(1, 2), (3, None)]
+
+    def test_multiple_items_with_size_and_pad(self) -> None:
+        chunked = list(chunks([1, 2, 3, 4, 5], size=3, pad=None))
+        assert chunked == [(1, 2, 3), (4, 5, None)]
 
     def test_multiple_pad_items_with_pad(self) -> None:
         chunked = list(chunks([None, None, None], pad=None))
