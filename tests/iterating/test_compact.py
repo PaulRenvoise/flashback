194e83cbb4750c58ccf53c720b232b0f97a83390
Paul Renvoisé <renvoisepaul@gmail.com>
2026-02-24 13:42:25 +0100

    feat(test_compact): add a few tests

--- tests/iterating/test_compact.py
+++ tests/iterating/test_compact.py
@@ -12,6 +12,16 @@ class TestCompact:
 
         assert compacted == [1, 2, 3, 4, 5]
 
+    def test_multiple_types(self) -> None:
+        compacted: list[int | str] = compact([1, None, "2", 3, None, 4, 5, None])
+
+        assert compacted == [1, "2", 3, 4, 5]
+
+    def test_keep_falsy_values(self) -> None:
+        compacted: list[int | str | bool] = compact([0, None, "", False, None])
+
+        assert compacted == [0, "", False]
+
     def test_only_none(self) -> None:
         compacted = compact([None, None, None])
 
